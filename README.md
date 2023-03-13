56f3c94f9e2f6f359e9b0cd0407428ac478464da
Your Name <you@example.com>
2023-03-12 20:59:17 -0700

    update file php

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kelas-php-day00-iven-lagowan
+
+
+##Belajar PHP & MySQL di Komonitas SaCode
+
 By. Iven Lagowan
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	// Variabel String
+	$judul = "Hello Word !!";
+	$bilangan = 20; 
+	$nama = "Iven";
+?>
+<html>
+	<head>
+		<title> PHP Day 00 </title>
+	</head>
+	<body>
+		<h1> PHP Day 00 - FILE PHP</h1>
+		<h2> <?php echo $judul; ?> </h2>
+		<h2> <?= $bilangan ?> </h2>
+		<h2> <?= "Hello ". $nama?> </h2>
+	<body>
+</html>
